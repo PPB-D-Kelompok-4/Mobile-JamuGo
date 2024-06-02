e90a2cc940c4eaf08ae851104ef937c3c9ece270
helmitaqiyudin <helmitaqiyudin@gmail.com>
2024-06-02 23:10:40 +0700

    feat: Add cart, order, and profile page & navigation and ui updates

---
+++ lib/pages/order_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class OrderPage extends StatefulWidget {
+  const OrderPage({super.key});
+
+  @override
+  State<OrderPage> createState() => _OrderPageState();
+}
+
+class _OrderPageState extends State<OrderPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Order"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () {
+              GoRouter.of(context).push('/cart');
+            },
+          ),
+        ],
+        backgroundColor: Colors.green,
+      ),
+      body: const Center(
+        child: Text('Order Page'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 1,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Menu',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Order',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        onTap: (index) {
+          if (index == 0) {
+            GoRouter.of(context).go('/home');
+          } else if (index == 1) {
+            GoRouter.of(context).go('/order');
+          } else if (index == 2) {
+            GoRouter.of(context).go('/profile');
+          }
+        },
+      ),
+    );
+  }
+}
