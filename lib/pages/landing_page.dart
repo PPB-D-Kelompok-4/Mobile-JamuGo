6dbedbe2f0c0f2718cdd058ee7d48130f5ed2f19
helmitaqiyudin <helmitaqiyudin@gmail.com>
2024-06-04 12:12:35 +0700

    chore: Refactor landing page UI layout and button styles

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -17,7 +17,7 @@ class _LandingPageState extends State<LandingPage> {
   }
 
   Future<String> getToken() async {
-    final token = await SecureStorage.readSecureData(key: 'token');
+    final token = await SecureStorageUtil.readSecureData(key: 'token');
     if (token != null) {
       GoRouter.of(context).go('/home');
     }
@@ -28,92 +28,88 @@ class _LandingPageState extends State<LandingPage> {
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(30),
-          child: Column(
-            children: [
-              Image.asset(
-                'assets/images/jamugologo.png',
-                height: height * 0.5,
-              ),
-              const Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text(
-                    'JamuGO',
-                    style: TextStyle(
-                      fontSize: 60,
-                      color: Color.fromRGBO(94, 185, 120, 1),
-                      fontWeight: FontWeight.bold,
-                      shadows: [
-                        Shadow(
-                          blurRadius: 5.0,
-                          color: Colors.black,
-                          offset: Offset(1.0, 1.0),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Text(
-                    'Kesehatan Tradisional dalam Genggaman Anda.',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Color.fromRGBO(0, 0, 0, 1),
-                      fontWeight: FontWeight.normal,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              ),
-              const SizedBox(height: 200),
-              Row(
-                children: [
-                  Expanded(
-                    child: OutlinedButton(
-                      onPressed: () {
-                        GoRouter.of(context).go('/login');
-                      },
-                      style: OutlinedButton.styleFrom(
-                        shape: const RoundedRectangleBorder(),
-                        backgroundColor: const Color.fromRGBO(94, 185, 120, 1),
-                        side: const BorderSide(
-                            color: Color.fromRGBO(94, 185, 120, 1)),
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 15,
-                        ),
+      body: Container(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Image.asset(
+                  'assets/images/jamugologo.png',
+                  height: height * 0.5,
+                ),
+                const Text(
+                  'JamuGO',
+                  style: TextStyle(
+                    fontSize: 60,
+                    color: Color.fromRGBO(94, 185, 120, 1),
+                    fontWeight: FontWeight.bold,
+                    shadows: [
+                      Shadow(
+                        blurRadius: 5.0,
+                        color: Colors.black,
+                        offset: Offset(1.0, 1.0),
                       ),
-                      child: const Text(
-                        'Login',
-                        style: TextStyle(color: Colors.white),
+                    ],
+                  ),
+                ),
+                const Text(
+                  'Kesehatan Tradisional dalam Genggaman Anda.',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Color.fromRGBO(0, 0, 0, 1),
+                    fontWeight: FontWeight.normal,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: OutlinedButton(
+                    onPressed: () {
+                      GoRouter.of(context).go('/login');
+                    },
+                    style: OutlinedButton.styleFrom(
+                      shape: const RoundedRectangleBorder(),
+                      backgroundColor: const Color.fromRGBO(94, 185, 120, 1),
+                      side: const BorderSide(
+                          color: Color.fromRGBO(94, 185, 120, 1)),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 15,
                       ),
                     ),
-                  ),
-                  const SizedBox(width: 10),
-                  Expanded(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        side: const BorderSide(color: Colors.black),
-                        shape: const RoundedRectangleBorder(),
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 15,
-                        ),
-                      ),
-                      onPressed: () {
-                        GoRouter.of(context).go('/register');
-                      },
-                      child: const Text(
-                        'Register',
-                        style:
-                            TextStyle(color: Color.fromRGBO(94, 185, 120, 1)),
-                      ),
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(color: Colors.white),
                     ),
                   ),
-                ],
-              ),
-            ],
-          ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      side: const BorderSide(color: Colors.black),
+                      shape: const RoundedRectangleBorder(),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 15,
+                      ),
+                    ),
+                    onPressed: () {
+                      GoRouter.of(context).go('/register');
+                    },
+                    child: const Text(
+                      'Register',
+                      style: TextStyle(color: Color.fromRGBO(94, 185, 120, 1)),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
